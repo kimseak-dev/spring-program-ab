cbb697b8f49e8e9b54b550dae97cbb2836b62e22
kimseak-dev <kimseakphon@gmail.com>
2019-10-18 16:33:16 +0700

    testing

--- src/main/java/org/alicebot/ab/MainSpring.java
+++ src/main/java/org/alicebot/ab/MainSpring.java
@@ -4,11 +4,11 @@ package org.alicebot.ab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MainSpring {
-
-    public static void main(String[] args) {
-        SpringApplication.run(MainSpring.class, args);
-    }
-
-}
+//@SpringBootApplication
+//public class MainSpring {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(MainSpring.class, args);
+//    }
+//
+//}
