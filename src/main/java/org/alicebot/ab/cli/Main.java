bdffd6db3f6ac0b71133db428ed1752e46dab07b
kimseak-dev <kimseakphon@gmail.com>
2019-10-18 16:38:30 +0700

    hi

--- src/main/java/org/alicebot/ab/cli/Main.java
+++ src/main/java/org/alicebot/ab/cli/Main.java
@@ -9,31 +9,31 @@ import org.slf4j.LoggerFactory;
 /**
  * Created by haggais on 3/01/2016.
  */
-public class Main {
-
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
-
-    public static void main(String args[]) {
-        String path = System.getProperty("user.dir");
-        String name = "alice2";
-        String action = "chat";
-
-        Bot bot = new Bot(name, path, action);
-        bot.brain.nodeStats();
-
-        Chat chatSession = new Chat(bot);
-
-        while (true) {
-            System.out.print("human: ");
-            String textLine = IOUtils.readInputTextLine();
-
-            if (textLine == null || textLine.equals("q")) {
-                bot.writeQuit();
-                System.exit(0);
-            }
-
-            String response = chatSession.multisentenceRespond(textLine);
-            System.out.println(name + ": " +  response);
-        }
-    }
-}
+//public class Main {
+//
+//    private static final Logger log = LoggerFactory.getLogger(Main.class);
+//
+//    public static void main(String args[]) {
+//        String path = System.getProperty("user.dir");
+//        String name = "alice2";
+//        String action = "chat";
+//
+//        Bot bot = new Bot(name, path, action);
+//        bot.brain.nodeStats();
+//
+//        Chat chatSession = new Chat(bot);
+//
+//        while (true) {
+//            System.out.print("human: ");
+//            String textLine = IOUtils.readInputTextLine();
+//
+//            if (textLine == null || textLine.equals("q")) {
+//                bot.writeQuit();
+//                System.exit(0);
+//            }
+//
+//            String response = chatSession.multisentenceRespond(textLine);
+//            System.out.println(name + ": " +  response);
+//        }
+//    }
+//}
